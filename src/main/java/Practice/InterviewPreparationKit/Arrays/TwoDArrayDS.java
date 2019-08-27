68373d621d3fd03d670fe23400fea47ad2391468
Sankar <snnov1980@gmail.com>
2019-08-26 22:31:53 -0500

    initial skeleton for 2D Array hourglass sum

---
+++ src/main/java/Practice/InterviewPreparationKit/Arrays/TwoDArrayDS.java
@@ -0,0 +1,40 @@
+package main.java.Practice.InterviewPreparationKit.Arrays;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class TwoDArrayDS {
+	// Complete the hourglassSum function below.
+    static int hourglassSum(int[][] arr) {
+    	int sum = 0;
+    	
+    	
+    	return sum;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        int[][] arr = new int[6][6];
+
+        for (int i = 0; i < 6; i++) {
+            String[] arrRowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int j = 0; j < 6; j++) {
+                int arrItem = Integer.parseInt(arrRowItems[j]);
+                arr[i][j] = arrItem;
+            }
+        }
+
+        int result = hourglassSum(arr);
+        System.out.println(result);
+
+        scanner.close();
+    }
+}
