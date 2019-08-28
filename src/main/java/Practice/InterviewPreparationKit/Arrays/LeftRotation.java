cfd21f85b73ca3d65915620daaf473cf4942cbd9
Sankara Nahak <sankara.nahak@wnco.com>
2019-08-28 16:51:55 -0500

    left rotation of array problem added

---
+++ src/main/java/Practice/InterviewPreparationKit/Arrays/LeftRotation.java
@@ -0,0 +1,53 @@
+package main.java.Practice.InterviewPreparationKit.Arrays;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class LeftRotation {
+	static int[] rotLeft(int[] a, int d) {
+		int[] finalArray = new int[a.length];
+
+		for (int i = d; i >= 0; i--) {
+			finalArray[d-i] = a[i];
+		}
+		return finalArray;
+	}
+
+	private static final Scanner scanner = new Scanner(System.in);
+
+	public static void main(String[] args) throws IOException {
+
+		String[] nd = scanner.nextLine().split(" ");
+
+		int n = Integer.parseInt(nd[0]);
+
+		int d = Integer.parseInt(nd[1]);
+
+		int[] a = new int[n];
+
+		String[] aItems = scanner.nextLine().split(" ");
+		scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+		for (int i = 0; i < n; i++) {
+			int aItem = Integer.parseInt(aItems[i]);
+			a[i] = aItem;
+		}
+
+		int[] result = rotLeft(a, d);
+
+		for (int i = 0; i < result.length; i++) {
+			System.out.print(String.valueOf(result[i]));
+
+			if (i != result.length - 1) {
+				System.out.println(" ");
+			}
+		}
+
+		scanner.close();
+	}
+}
