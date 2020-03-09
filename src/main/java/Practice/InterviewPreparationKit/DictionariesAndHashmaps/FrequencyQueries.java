dd696326374a1762e1f60e3f42f2a5463b543f89
Sankara Nahak <sankara.nahak@wnco.com>
2020-03-09 16:37:21 -0500

    Solved the frequency problem with perf issue

--- src/main/java/Practice/InterviewPreparationKit/DictionariesAndHashmaps/FrequencyQueries.java
+++ src/main/java/Practice/InterviewPreparationKit/DictionariesAndHashmaps/FrequencyQueries.java
@@ -15,12 +15,37 @@ import static java.util.stream.Collectors.toList;
 
 public class FrequencyQueries {
 	static List<Integer> freqQuery(List<List<Integer>> queries) {
-
+		List<Integer> output = new ArrayList<Integer>();
+		List<Integer> tempArray = new ArrayList<Integer>();
+		for(List<Integer> temp : queries) {
+			if((temp.get(0))==1) {
+				tempArray.add(temp.get(1));
+			}else if((temp.get(0))==2) {
+				tempArray.remove(temp.get(1));
+			}else if((temp.get(0))==3) {
+				if(tempArray.size()>0) {
+					Set<Integer> distinct = new HashSet<>(tempArray);
+					output.add(0);
+					for(Integer i : distinct) {
+						if(Collections.frequency(tempArray, i)==temp.get(1)) {
+							output.remove(output.size()-1);
+							output.add(1);
+							break;
+						}
+					}	
+				}else {
+					output.add(0);
+					continue;
+				}
+			}
+		}
+		
+		return output;
 	}
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+		BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		int q = Integer.parseInt(bufferedReader.readLine().trim());
 
